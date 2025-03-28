ab9a36a9339359da03b6f3a15ec736dfc14dcda4
daniellamct <daniellamisthebest@gmail.com>
2025-03-28 16:38:12 +0800

    Implement news summarization function and an additional sentiment analysis module

--- News Sentiment Analysis/NewsSentiment.py
+++ News Sentiment Analysis/NewsSentiment.py
@@ -1,12 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from textblob import TextBlob
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+from scipy.special import softmax
+from summarizer import Summarizer
+
+# Load the pretrained model
+task = 'sentiment-latest'
+model_dir = f'News Sentiment Analysis/cardiffnlp/twitter-roberta-base-{task}'
+tokenizer = AutoTokenizer.from_pretrained(model_dir)
+model = AutoModelForSequenceClassification.from_pretrained(model_dir)
+
+# Load the summarizer
+model_summarizer = Summarizer()
+
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
 
 # Web Scraping
 stock = "AAPL"
 url = f'https://finance.yahoo.com/quote/{stock}/news/?p={stock}'
-driver = webdriver.Chrome()  
+driver = webdriver.Chrome(options=chrome_options)  
 driver.get(url)
+print("Fetched the News menu")
 file = driver.page_source
 soup = BeautifulSoup(file, 'html.parser')
 
@@ -20,8 +42,10 @@ for li in li_li:
 
 titles = []
 paragraphs = []
-articles = []
-sentiments = []
+paragraphs_500 = []
+paragraphs_100 = []
+sentiments_Textblob = []
+sentiments_Cardiffnlp = []
 for i in range(0, len(links)):
     title = "" 
     paragraph = ""
@@ -30,6 +54,7 @@ for i in range(0, len(links)):
         # Web Scraping for each article
         url = links[i]
         driver.get(url)
+        print(f'Fetched News: {str(i+1)}')
         file = driver.page_source
         soup = BeautifulSoup(file, 'html.parser')
 
@@ -44,20 +69,56 @@ for i in range(0, len(links)):
             p_all = text_area.find_all('p')
             for p in p_all:
                 paragraph += p.text
+        if len(paragraph) < 1000:
+            print("Skipped news: The Content length is too short. ")
+            continue
 
         if(len(title) != 0 and len(paragraph) != 0):
-            # Sentiment Analysis
-            article = f'{title}. {paragraph}'
-            analysis = TextBlob(article)
+            # Summarize
+            paragraph_500 = model_summarizer(paragraph, ratio=0.8)
+            paragraph_100 = model_summarizer(paragraph, ratio=0.2)
+
+            # Sentiment Analysis (TextBlob)
+            analysis = TextBlob(f'{title}. {paragraph}')
+            if(analysis.sentiment.polarity > 0.2):
+                result_TextBlob = f'Positive (with {analysis.sentiment.polarity:.2f} polarity score)'
+            elif(analysis.sentiment.polarity < -0.2):
+                result_TextBlob = f'Negative (with {analysis.sentiment.polarity:.2f} polarity score)'
+            else:
+                result_TextBlob = f'Neutral (with {analysis.sentiment.polarity:.2f} polarity score)'
+
+            # Sentiment Analysis (Cardiffnlp)
+            encoded_input = tokenizer(f'{title}. {paragraph_500}', return_tensors='pt')
+            output = model(**encoded_input)
+            scores = output[0][0].detach().numpy()
+            scores = softmax(scores)
+            if(scores[0] > scores[1]) and (scores[0] > scores[2]):
+                # Positive
+                result_Cardiffnlp = f'Positive (with {scores[0]*100:.2f}% probability)'
+            elif(scores[1] > scores[0]) and (scores[1] > scores[2]):
+                # Neutral
+                result_Cardiffnlp = f'Neutral (with {scores[1]*100:.2f}% probability)'
+            else:
+                # Negative
+                result_Cardiffnlp = f'Negative (with {scores[2]*100:.2f}% probability)'
 
             titles.append(title)
             paragraphs.append(paragraph)
-            articles.append(article)
-            sentiments.append(analysis.sentiment.polarity)
+            paragraphs_100.append(paragraph_100)
+            paragraphs_500.append(paragraph_500)
+            sentiments_Textblob.append(result_TextBlob)
+            sentiments_Cardiffnlp.append(result_Cardiffnlp)
+            # Update the GUI here
+
     except Exception as exception:
-        print(f'Skipped\n')
+        print(f'Skipped')
+        print(f'{exception}\n')
 
+# These contents below will be shown in the GUI
 for i in range(0, len(titles)):
-    print(f'\n{i+1}: {titles[i]}: ')
-    print(f'Rating: {sentiments[i]}\n')
-
+    print(f'\n\n{i+1}: {titles[i]}: ')
+    print(f'Rating (By TextBlob): {sentiments_Textblob[i]}')
+    print(f'Rating (By Cardiffnlp): {sentiments_Cardiffnlp[i]}')
+    #print(f'Origin Article: {paragraphs_500[i]}\n')
+    #print(f'Summarization (Within 500 words): {paragraphs_500[i]}\n')
+    #print(f'Summarization (Within 100 words): {paragraphs_100[i]}\n')
